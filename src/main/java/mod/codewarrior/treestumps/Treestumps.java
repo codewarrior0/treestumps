fadcddbee7f2ce44c16e69a2fa29877d9bad89ec
David Vierra <codewarrior@hawaii.rr.com>
2018-07-01 17:57:30 -1000

    Update to minecraft 1.12.2

--- src/main/java/mod/codewarrior/treestumps/Treestumps.java
+++ src/main/java/mod/codewarrior/treestumps/Treestumps.java
@@ -24,7 +24,7 @@ import java.util.List;
 public class Treestumps
 {
     public static final String MODID = "treestumps";
-    public static final String VERSION = "2.0.0";
+    public static final String VERSION = "$$VERSION$$";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
